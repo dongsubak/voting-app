e14e2760c2c4360cbcf13d4bc50907e33b7ecd10
unknown <blackxub@naver.com>
2018-09-08 17:16:38 +0900

    'app-1.js'

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, basic React component.
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
