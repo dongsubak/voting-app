5a8287aaf89d8ad61b07266cc8c0d0fa2fccb3c8
unknown <blackxub@naver.com>
2018-09-09 21:24:28 +0900

    app-7,8

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,27 @@ class ProductList extends React.Component {
 
     this.state = {
       products: [],
-    }
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
   componentDidMount() {
     this.setState({ products: Seed.products});
   }
-  
+
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product)=> {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    })
   }
   render() {
     const product = Seed.products[0];
